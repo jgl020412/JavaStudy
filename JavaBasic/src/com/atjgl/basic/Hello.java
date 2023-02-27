ff7e3be83dff1f1483704cd4b04a964a5b78d542
jgl_0412 <2051540478@qq.com>
2023-02-27 19:28:23 +0800

    添加Java基础部分

---
+++ JavaBasic/src/com/atjgl/basic/Hello.java
@@ -0,0 +1,11 @@
+package com.atjgl.basic;
+
+/**
+ * @author 小亮
+ **/
+public class Hello {
+    // 这是梦开始的地方，学习Java时，敲下的第一行代码
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
